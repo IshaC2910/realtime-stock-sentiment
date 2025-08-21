1f84d200dfd3218cdbeab9b27a6217d9c78f2536
IshaC2910 <111347253+IshaC2910@users.noreply.github.com>
2025-08-21 22:19:17 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,10 +1,5 @@
 import os
 from dotenv import load_dotenv
-# src/config.py
-import streamlit as st
-
-NEWS_API_KEY = st.secrets["api_keys"]["newsapi"]
-TWITTER_BEARER = st.secrets["api_keys"]["twitter_bearer"]
 
 load_dotenv()
 
